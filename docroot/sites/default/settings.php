bf3f3d094c43a41f9432e5eb634410668f19655a
Aubrie Hill <ahill@elevatedthird.com>
2018-06-05 19:17:27 -0600

    Updated settings for server

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -105,17 +105,79 @@ $config['']['account'] = '';
 // Disable dev modules on all environments by default.
 $config['config_split.config_split.config_dev']['status'] = FALSE;
 
-// If $_ENV['AH_SITE_ENVIRONMENT'], load Acquia settings.
-if(isset($_ENV['AH_SITE_ENVIRONMENT'])) {
-  if (file_exists(__DIR__ . '/settings.acquia.php')) {
-    include __DIR__ . '/settings.acquia.php';
-
-  }
-}
 // If $_SERVER['AH_SITE_ENVIRONMENT'], load Blackmesh settings.
-elseif(isset($_SERVER['AH_SITE_ENVIRONMENT'])) {
-  if (file_exists(__DIR__ . '/settings.blackmesh.php')) {
-    include __DIR__ . '/settings.blackmesh.php';
+if(isset($_SERVER['AH_SITE_ENVIRONMENT'])) {
+
+  $config['system.performance']['css']['preprocess'] = TRUE;
+  $config['system.performance']['js']['preprocess'] = TRUE;
+
+  // Set GTM Code default
+  $config['e3_google_tag.settings']['gtm_code'] = '';
+
+  $config['environment_indicator.indicator']['bg_color'] = '#930007';
+  $config['environment_indicator.indicator']['fg_color'] = '#ffffff';
+  $config['environment_indicator.indicator']['name'] = 'BlackMesh ' . $_ENV['AH_SITE_ENVIRONMENT'];
+
+  // Set trusted host pattern for the acquia paragon site. We need to set this because we cannot add additional
+  // aliases to a free acquia account. This can be deleted for any new project created from paragon.
+  $settings['trusted_host_patterns'][] = 'bhk-d8.dev.e3develop.com';
+  $settings['trusted_host_patterns'][] = 'bhk-d8.prod.e3develop.com';
+  $settings['trusted_host_patterns'][] = 'hawkincentives.com';
+
+  /**
+   * Set default config_readonly status to TRUE on all Acquia environments.
+   */
+  //if (PHP_SAPI !== 'cli') {
+  //  $settings['config_readonly'] = FALSE;
+  //}
+
+  switch ($_ENV['AH_SITE_ENVIRONMENT']) {
+    case 'dev':
+      // Configure shield for dev environment.
+      $config['shield.settings']['user'] = 'hawkincentives';
+      $config['shield.settings']['pass'] = '3ditHawk';
+
+      $settings['file_private_path'] = "/var/www/bhk-d8.dev.e3develop.com/application/shared/private";
+      $config['system.file']['path']['temporary'] = "/var/www/bhk-d8.dev.e3develop.com/application/shared/private/tmp";
+
+      /**
+       * Load the development services definition file.
+       */
+      $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+
+      /**
+       * Master DB and Config Read-Only settings
+       *
+       * Set the environment indicator for the environment with the Master DB.  This should never be on more than one DB.
+       * If non-developers are allowed to modify configuration on the master environment, add the following line:
+       *
+       * $settings['config_readonly'] = FALSE;
+       *
+       * NOTE: If set to FALSE, caution should be used when merging in config changes.
+       * All Master DB config must be merged into the master branch before merging new config from VCS.
+       *
+       */
+      $config['environment_indicator.indicator']['name'] = 'Acquia ' . $_ENV['AH_SITE_ENVIRONMENT'] . ' [Master DB]';
+      $config['environment_indicator.indicator']['bg_color'] = '#000000';
+
+      break;
+    case 'prod':
+
+      //@TODO remove before launch
+      $config['shield.settings']['user'] = 'hawkincentives';
+      $config['shield.settings']['pass'] = '3ditHawk';
+
+      //@TODO Configure this
+      //    $settings['file_private_path'] = "/var/www/bhk-d8.dev.e3develop.com/application/shared/private";
+      //    $config['system.file']['path']['temporary'] = "/var/www/bhk-d8.dev.e3develop.com/application/shared/private/tmp";
+
+      // Set logging level on production.
+      $config['system.logging']['error_level'] = 'hide';
+
+      //@TODO update before launch
+      // Set GTM Code
+      $config['e3_google_tag.settings']['gtm_code'] = '';
+      break;
   }
 }
 // If drupal-vm settings exist, load them.
