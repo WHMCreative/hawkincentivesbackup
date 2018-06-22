e413681e7e52a62c778552c5f206ecef55c89272
Vlad Nosykov <vladyslav.nosikov@lemberg.co.uk>
2018-06-22 15:54:34 +0300

    Adjust Deploykit for Prod

--- docroot/sites/default/settings.blackmesh.php
+++ docroot/sites/default/settings.blackmesh.php
@@ -98,8 +98,8 @@ switch ($_ENV['AH_SITE_ENVIRONMENT']) {
     $config['shield.settings']['pass'] = '3ditHawk';
 
     //@TODO Configure this
-//    $settings['file_private_path'] = "/var/www/bhk-d8.dev.e3develop.com/application/shared/private";
-//    $config['system.file']['path']['temporary'] = "/var/www/bhk-d8.dev.e3develop.com/application/shared/private/tmp";
+    $settings['file_private_path'] = "/var/www/bhk-d8.dev.e3develop.com/application/shared/private";
+    $config['system.file']['path']['temporary'] = "/var/www/bhk-d8.dev.e3develop.com/application/shared/private/tmp";
 
     // Set logging level on production.
     $config['system.logging']['error_level'] = 'hide';
