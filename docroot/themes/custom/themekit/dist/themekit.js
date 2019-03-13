f6b8cbbaca28f9074ec87eabfebed48f64767f7c
Aubrie Hill <ahill@elevatedthird.com>
2019-03-13 16:02:05 -0600

    Updated stk location

--- docroot/themes/custom/themekit/dist/themekit.js
+++ docroot/themes/custom/themekit/dist/themekit.js
@@ -10959,17 +10959,13 @@ window.addEventListener('message', function (e) {
  * Add Dialog Tech Script
  */
 
-Drupal.behaviors.bahaviorName = {
-  attach: function attach(context, settings) {
-    var _stk = "30c910f30941c2bad88b097619a5519b04973829";
-    (function () {
-      var a = document,
-          b = a.createElement("script");b.type = "text/javascript";
-      b.async = !0;b.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'd31y97ze264gaa.cloudfront.net/assets/st/js/st.js';
-      a = a.getElementsByTagName("script")[0];a.parentNode.insertBefore(b, a);
-    })();
-  }
-};
+window._stk = "30c910f30941c2bad88b097619a5519b04973829";
+(function () {
+  var a = document,
+      b = a.createElement("script");b.type = "text/javascript";
+  b.async = !0;b.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'd31y97ze264gaa.cloudfront.net/assets/st/js/st.js';
+  a = a.getElementsByTagName("script")[0];a.parentNode.insertBefore(b, a);
+})();
 
 /***/ }),
 
--- docroot/themes/custom/themekit/js/src/dialog-tech.js
+++ docroot/themes/custom/themekit/js/src/dialog-tech.js
@@ -3,14 +3,10 @@
  * Add Dialog Tech Script
  */
 
-Drupal.behaviors.bahaviorName = {
-  attach(context, settings) {
-    var _stk = "30c910f30941c2bad88b097619a5519b04973829";
-    (function(){
-      var a=document, b=a.createElement("script"); b.type="text/javascript";
-      b.async=!0; b.src=('https:'==document.location.protocol ? 'https://' :
-        'http://') + 'd31y97ze264gaa.cloudfront.net/assets/st/js/st.js';
-      a=a.getElementsByTagName("script")[0]; a.parentNode.insertBefore(b,a);
-    })();
-  },
-};
+window._stk = "30c910f30941c2bad88b097619a5519b04973829";
+(function(){
+  var a=document, b=a.createElement("script"); b.type="text/javascript";
+  b.async=!0; b.src=('https:'==document.location.protocol ? 'https://' :
+    'http://') + 'd31y97ze264gaa.cloudfront.net/assets/st/js/st.js';
+  a=a.getElementsByTagName("script")[0]; a.parentNode.insertBefore(b,a);
+})();
